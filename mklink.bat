2b621362389baafc437dda0e55dbc187516ab784
AnotherEnd <210379417@qq.com>
2022-09-10 20:17:11 +0800

    update

---
+++ mklink.bat
@@ -0,0 +1,5 @@
+cd /d %~dp0
+mklink /j "E:\Github\AEAssistRelease\CNVersion" "E:\Github\Dalamud_AEAssist\AEAssist\Output\AEAssist\CNVersion"
+mklink /j "E:\Github\AEAssistRelease\ENVersion" "E:\Github\Dalamud_AEAssist\AEAssist\Output\AEAssist\ENVersion"
+mklink /j "E:\Github\AEAssistRelease\AETriggerLineEditor" "E:\Github\Dalamud_AEAssist\AEAssist\Output\AETriggerLineEditor"
+pause
